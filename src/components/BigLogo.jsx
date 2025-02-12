85d476f54b63b1071fee3d7fb5962129f95295ac
Rafael Niebles <rnieblesealo@gmail.com>
2025-02-12 09:54:29 -0500

    replace logo for gif for iphone functionality

--- src/components/BigLogo.jsx
+++ src/components/BigLogo.jsx
@@ -1,14 +1,10 @@
 export default function BigLogo() {
   return (
     <div className="w-full pointer-events-none">
-      <video
-        src="/logo.mp4"
+      <img
+        src="/mw-visual.gif"
         className="bg-black w-full h-[70vh] object-cover"
         autoPlay
-        loop
-        muted
-        playsInline
-        disablePictureInPicture
       />
     </div>
   )
