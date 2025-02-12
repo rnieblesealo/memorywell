e2c208427931ae463d782be2cab55f2545fa9bb0
Rafael Niebles <rnieblesealo@gmail.com>
2025-02-11 22:45:12 -0500

    add burgermenu; needs links

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -1,5 +1,7 @@
 import clsx from "clsx"
 import { Link } from "react-router-dom"
+import { GiHamburgerMenu } from "react-icons/gi";
+import { UsePageContext } from "../util/PageContext"
 
 export default function Navigator() {
   const text = clsx(
@@ -24,6 +26,37 @@ export default function Navigator() {
     "hover:text-purple-400",
   )
 
+  const Menu = () => (
+    <ul className={`${text} ${flexRow} ml-auto gap-10`}>
+      <li className={selectBorder}>
+        <Link to="/" className={selectBorder}>Home</Link>
+      </li>
+      <li className={selectBorder}>
+        <Link to="/about" className={selectBorder}>About</Link>
+      </li>
+      <li className={selectBorder}>
+        <Link to="/music" className={selectBorder}>Music</Link>
+      </li>
+      <li className={selectBorder}>
+        <Link className={selectBorder}>Merch</Link>
+      </li>
+    </ul>
+  )
+
+  const BurgerButton = () => (
+    <button
+      className="ml-auto text-white text-[28px] cursor-pointer"
+      onClick={() => {
+        context.enableFullscreenNavigator(true)
+        document.body.style.overflow = "hidden";
+      }}
+    >
+      <GiHamburgerMenu />
+    </button>
+  )
+
+  const context = UsePageContext()
+
   return (
     <nav className={`${flexRow} p-4  h-min-content`}>
       <Link to="/" className="transition-opacity duration-[0.2s] hover:opacity-50">
@@ -34,20 +67,7 @@ export default function Navigator() {
           height="50px"
         />
       </Link>
-      <ul className={`${text} ${flexRow} ml-auto gap-10`}>
-        <li className={selectBorder}>
-          <Link to="/" className={selectBorder}>Home</Link>
-        </li>
-        <li className={selectBorder}>
-          <Link to="/about" className={selectBorder}>About</Link>
-        </li>
-        <li className={selectBorder}>
-          <Link to="/music" className={selectBorder}>Music</Link>
-        </li>
-        <li className={selectBorder}>
-          <Link className={selectBorder}>Merch</Link>
-        </li>
-      </ul>
+      {context.burgerButton ? <BurgerButton /> : <Menu />}
     </nav>
   )
 }
--- src/util/PageContext.jsx
+++ src/util/PageContext.jsx
@@ -1,17 +1,18 @@
-import { createContext } from "react";
+import { useContext, createContext } from "react";
+import { useState, useEffect } from "react"
 import PropTypes from "prop-types"
-
 import Navigator from "../components/Navigator"
 import LinkButton from "../components/LinkButton"
 import Footer from "../components/Footer"
-
 import { FaHeart } from "react-icons/fa6"
 import { FaYoutube } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaTiktok } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
+import { IoMdClose } from "react-icons/io";
 
 import clsx from "clsx"
+
 
 const PageContext = createContext(null);
 
@@ -25,20 +26,63 @@ export default function TimerContextProvider({ children }) {
     "gap-10"
   )
 
+  const FullscreenNavigator = () => (
+    <div className="w-screen h-screen bg-black text-white text-[28px] fixed inset-0 z-10">
+      <button onClick={() => {
+        enableFullscreenNavigator(false)
+        document.body.style.overflow = "visible";
+
+      }}>
+        <IoMdClose className="animate-jump-in text-[32px] absolute right-0 m-4" />
+      </button>
+      <ul className="w-full h-full flex flex-col items-center justify-top p-12">
+        <li className="animate-fade-right p-5">Home</li>
+        <li className="animate-fade-right p-5">About</li>
+      </ul>
+    </div>
+  )
+
+  // useful context-related info
+  const [fullscreenNavigator, enableFullscreenNavigator] = useState(false)
+  const [burgerButton, enableBurgerButton] = useState(true)
+
+  const contextInfo = {
+    fullscreenNavigator,
+    enableFullscreenNavigator,
+    burgerButton,
+    enableBurgerButton
+  }
+
+  // switch to burger menu on small screen
+  useEffect(() => {
+    // make and add listener
+    const onResize = () => {
+      enableBurgerButton(window.innerWidth < 500)
+    }
+
+    window.addEventListener("resize", onResize)
+
+    // remove listener
+    return () => {
+      window.removeEventListener("resize", onResize)
+    }
+  }, [])
+
   return (
     <>
-      <PageContext.Provider>
+      <PageContext.Provider value={contextInfo}>
+        {fullscreenNavigator && <FullscreenNavigator />}
         <Navigator />
         <div className={main}>
           {children}
         </div>
-        <Footer className="flex ">
+        <Footer className="flex">
           <LinkButton icon={<FaYoutube />} useIconBg />
           <LinkButton icon={<FaInstagram />} useIconBg />
           <LinkButton icon={<FaTiktok />} useIconBg />
           <LinkButton icon={<FaTwitter />} useIconBg />
         </Footer>
-      </PageContext.Provider>
+      </PageContext.Provider >
       <div className="flex items-center justify-center">
         <span className="flex flex-row items-center gap-2 text-white font-tiny5">Made by Rafa with <FaHeart color="#F90605" /></span>
       </div>
@@ -46,9 +90,8 @@ export default function TimerContextProvider({ children }) {
   )
 }
 
-// retrieve context for data purposes
-/*
-export function getPageContext() {
+// handy context get function
+export function UsePageContext() {
   const context = useContext(PageContext)
 
   if (!context) {
@@ -57,7 +100,6 @@ export function getPageContext() {
 
   return context;
 }
-*/
 
 TimerContextProvider.propTypes = {
   children: PropTypes.node
