e6dd745e7dd8db3ee3f326a421ce7730182f844b
Rafael Niebles <rnieblesealo@gmail.com>
2025-06-15 21:51:03 -0400

    propofol draft

--- src/util/hero.jsx
+++ src/util/hero.jsx
@@ -5,6 +5,38 @@ import { useState } from "react"
 
 import { FaSpotify } from "react-icons/fa"
 import { SiApplemusic } from "react-icons/si"
+import { FaYoutube } from "react-icons/fa6"
+
+const StreamLink = ({ url, icon }) => {
+  const ctx = UsePageContext()
+
+  const [hovered, setHovered] = useState(false)
+
+  return (
+    <Link
+      to={url}
+      target="_blank"
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      className="max-w-15 text-md h-min font-liter text-white rounded-lg p-2 flex items-center mx-1"
+      style={{
+        border: hovered ? `1px solid ${ctx.currentStyle?.accentColor}` : "1px solid white",
+        color: hovered ? ctx.currentStyle?.accentColor : "white",
+        transition: "border 0.2s ease, color 0.2s ease",
+      }}>
+      Stream on
+      <span className="text-2xl ml-2">
+        {icon}
+      </span>
+    </Link>
+  )
+}
+
+StreamLink.propTypes = {
+  url: PropTypes.string,
+  icon: PropTypes.node,
+  text: PropTypes.string
+}
 
 export const NormalLogo = () => (
   <div className="w-full pointer-events-none">
@@ -17,36 +49,6 @@ export const NormalLogo = () => (
 )
 
 export const AlmostRealLogo = () => {
-  const ctx = UsePageContext()
-
-  const StreamLink = ({ url, icon }) => {
-    const [hovered, setHovered] = useState(false)
-
-    return (
-      <Link
-        to={url}
-        target="_blank"
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
-        className="max-w-15 text-md h-min font-liter text-white rounded-lg p-2 flex items-center mx-1"
-        style={{
-          border: hovered ? `1px solid ${ctx.currentStyle?.accentColor}` : "1px solid white",
-          color: hovered ? ctx.currentStyle?.accentColor : "white",
-          transition: "border 0.2s ease, color 0.2s ease",
-        }}>
-        Stream on
-        <span className="text-2xl ml-2">
-          {icon}
-        </span>
-      </Link>
-    )
-  }
-
-  StreamLink.propTypes = {
-    url: PropTypes.string,
-    icon: PropTypes.node
-  }
-
   return (
     <div className="w-full relative h-[70vh] flex items-center justify-center relative" >
       <img
@@ -64,6 +66,26 @@ export const AlmostRealLogo = () => {
         <div className="flex">
           <StreamLink url="https://open.spotify.com/album/1LNTWgrNWucP77V2AXNk4M" icon={<FaSpotify />} />
           <StreamLink url="https://music.apple.com/us/album/almost-real-ep/1805090561" icon={<SiApplemusic />} />
+        </div>
+      </div>
+    </div >
+  )
+}
+
+export const PropofolLogo = () => {
+  return (
+    <div className="w-full relative h-[70vh] flex items-center justify-center relative" >
+      <img
+        src="/mw-visual-propofol.gif"
+        className="bg-black w-full h-full object-cover absolute pointer-events-none"
+        autoPlay
+      />
+      {/* Title card and stream link */}
+      <div className="relative w-full h-full flex flex-col items-center justify-center mix-blend-difference">
+        <h1 className="mb-4 text-white font-instrument font-light text-7xl sm:text-8xl lg:text-9xl">Propofol</h1>
+        <h2 className="mb-[50px] text-white font-instrument font-bold text-italic text-base sm:text-lg lg:text-xl">Music video out now</h2>
+        <div className="flex">
+          <StreamLink url="https://www.youtube.com/watch?v=B6LMETZJTIc" icon={<FaYoutube />} />
         </div>
       </div>
     </div >
